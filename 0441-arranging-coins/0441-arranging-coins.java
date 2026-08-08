7c85591cbe41173ffda02f675629dd9df2cc8f40
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-08 23:50:11 +0530

    Time: 1 ms (100%), Space: 43 MB (9.85%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,19 +1,16 @@
 class Solution {
     public int arrangeCoins(int n) {
-        int low = 1;
-        int high = n;
-        int index = 0;
+        long low = 1,high = n;
         while (low <=high){
-            int mid = low + (high - low)/2;
+            long mid = low + (high - low)/2;
             // make mid to long if not multiplication overflows before the result is assigned to long...
-            long sum = ((long)mid*((long)mid+1))/2;
-            if(sum == n) return mid;
+            long sum = mid*(mid+1)/2;
+            if(sum == n) return (int)mid;
             if(sum > n ) high = mid -1;
             else { 
-                index = mid;
             low = mid +1;
             }
         }    
-        return index;    
+        return (int)high;    
     }
 }
