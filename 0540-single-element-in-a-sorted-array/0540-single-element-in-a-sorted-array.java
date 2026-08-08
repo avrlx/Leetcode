61ed53ea323c85f8c8c06eaee83ad562548c1820
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-08 22:49:32 +0530

    Time: 0 ms (100%), Space: 53 MB (39.62%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,21 +1,32 @@
 class Solution {
     public int singleNonDuplicate(int[] arr) {
         int n = arr.length;
+        //one element
         if (n==1) return arr[0];
+        // if first is single
         if(arr[0] != arr[1]) return arr[0];
+        // if last is single
         if(arr[n-1] != arr[n-2] ) return arr[n-1];
+
         int low = 0;
         int high = n-1;
         while(low<=high){
             int mid = low +(high -low)/2;
+            // if mid is single 
             if(arr[mid] != arr[mid-1] && arr[mid] != arr[mid + 1]) return arr[mid];
             int f = mid;
             int s = mid;
+            // if mid is second element
             if(arr[mid] == arr[mid -1] ) f= mid-1;
+            // if mid is first element
             else s = mid +1;
+            //elements in left side
             int first = f- low;
+            // elements in right side
             int second = high - s;
+            //if left is even
             if(first %2==0) low = s +1;
+            // if right is even
             else high = f -1; 
         }
         return 1;
