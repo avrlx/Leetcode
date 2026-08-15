adc1e5b57d461c953a3d713ed2ea27a746ce17e9
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-15 16:58:06 +0530

    Time: 0 ms (100%), Space: 44.5 MB (57.9%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -4,13 +4,13 @@ class Solution {
         int high = arr.length -1;
         while(low<=high){
             int mid = low +( high - low)/2;
-            int orig = mid + 1;
-            int missing = arr[mid] - orig;
-            if(missing >= k) high = mid -1;
+            int orig = mid + 1; // original number has to be at mid
+            int missing = arr[mid] - orig; // no of missing numbers = missing
+            if(missing >= k) high = mid -1; // if missing >= k go left 
             else {
-                low = mid +1;
+                low = mid +1; // if missing < k go right
             }
         }
-        return high +1+ k;
+        return high +1+ k; // b/c while loop always at (low = high + 1) in Binary search...
     }
 }
