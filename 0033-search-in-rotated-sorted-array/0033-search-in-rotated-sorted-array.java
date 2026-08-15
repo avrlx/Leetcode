b141e9d5f7a7fad4aebd6058496a7ee14f37700f
avrlx <216227742+avrlx@users.noreply.github.com>
2026-08-15 12:46:19 +0530

    Time: 0 ms (100%), Space: 43.6 MB (82.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -4,14 +4,14 @@ class Solution {
         int high= arr.length - 1;
         while (low <= high){
             int mid = low + (high - low )/2;
-            if(arr[mid] == key) return mid;
-            else if(arr[low] <= arr[mid]){
-                if(arr[low] <= key && arr[mid] > key) high = mid -1;
-                else low = mid +1;
+            if(arr[mid] == key) return mid;// if element element is found..
+            else if(arr[low] <= arr[mid]){ // if left is sorted
+                if(arr[low] <= key && arr[mid] > key) high = mid -1; // if found in left 
+                else low = mid +1; // else right
             }
-            else{
-                if(arr[high] >= key && arr[mid] < key) low = mid +1;
-                else high = mid -1;
+            else{ // if right is sorted
+                if(arr[high] >= key && arr[mid] < key) low = mid +1; // if found in right
+                else high = mid -1; // else left
             }
         }
         return -1;
