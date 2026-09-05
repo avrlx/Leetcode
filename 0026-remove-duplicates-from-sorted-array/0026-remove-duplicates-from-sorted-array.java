902a4db73457321ba83f9cd1386afb4e780b1eb0
avrlx <216227742+avrlx@users.noreply.github.com>
2026-09-05 11:29:53 +0530

    Time: 1 ms (77.44%), Space: 47 MB (9.14%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,23 @@
 class Solution {
     public int removeDuplicates(int[] a) {
-        List<Integer> arr = new ArrayList<>(); // using extra space..
-        for (int i = 0; i < a.length - 1; i++) {
-            if (a[i] != a[i + 1])
-                arr.add(a[i]); // put different in arraylist
+        // List<Integer> arr = new ArrayList<>(); // using extra space..
+        // for (int i = 0; i < a.length - 1; i++) {
+        //     if (a[i] != a[i + 1])
+        //         arr.add(a[i]); // put different in arraylist
+        // }
+        // arr.add(a[a.length - 1]);
+        // for (int i = 0; i < arr.size(); i++) {
+        //     a[i] = arr.get(i); // update array
+        // }
+        // return arr.size();
+
+        int j =0;
+        for (int i = 1; i < a.length ; i++) {
+            if (a[j] != a[i])
+                a[++j] = a[i];
         }
-        arr.add(a[a.length - 1]);
-        for (int i = 0; i < arr.size(); i++) {
-            a[i] = arr.get(i); // update array
-        }
-        return arr.size();
+        return ++j;
+
+        
     }
 }
